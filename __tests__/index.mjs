35be286f6914f106551ddebee3cb6176494623d8
Juliette Pretot <hi@juliette.sh>
2022-04-13 20:32:58 +0100

    apply deno formatter

--- __tests__/index.mjs
+++ __tests__/index.mjs
@@ -1,28 +1,28 @@
-import Jasmine from 'jasmine'
-import JasmineConsoleReporter from 'jasmine-console-reporter'
-import * as AgnosticAxe from '../src/index.mjs'
+import Jasmine from "jasmine";
+import JasmineConsoleReporter from "jasmine-console-reporter";
+import * as AgnosticAxe from "../src/index.mjs";
 
-const jasmine = new Jasmine()
+const jasmine = new Jasmine();
 
-jasmine.env.clearReporters()
+jasmine.env.clearReporters();
 jasmine.addReporter(
   new JasmineConsoleReporter({
     colors: true,
     cleanStack: true,
     verbosity: 4,
-    listStyle: 'indent',
-    activity: false
-  })
-)
+    listStyle: "indent",
+    activity: false,
+  }),
+);
 
-jasmine.env.describe('The AgnosticAxe module', () => {
-  jasmine.env.it('should export a `logViolations` function', () => {
-    expect(typeof AgnosticAxe.logViolations).toBe('function')
-  })
+jasmine.env.describe("The AgnosticAxe module", () => {
+  jasmine.env.it("should export a `logViolations` function", () => {
+    expect(typeof AgnosticAxe.logViolations).toBe("function");
+  });
 
-  jasmine.env.it('should export a `AxeObserver` constructor', () => {
-    expect(typeof AgnosticAxe.AxeObserver).toBe('function')
-  })
-})
+  jasmine.env.it("should export a `AxeObserver` constructor", () => {
+    expect(typeof AgnosticAxe.AxeObserver).toBe("function");
+  });
+});
 
-jasmine.execute()
+jasmine.execute();
--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,8 @@
 import _AxeObserver, {
-  axeCoreInstance as _axeCoreInstance
-} from './AxeObserver.mjs'
-import _logViolations from './logViolations.mjs'
+  axeCoreInstance as _axeCoreInstance,
+} from "./AxeObserver.mjs";
+import _logViolations from "./logViolations.mjs";
 
-export const AxeObserver = _AxeObserver
-export const axeCoreInstance = _axeCoreInstance
-export const logViolations = _logViolations
+export const AxeObserver = _AxeObserver;
+export const axeCoreInstance = _axeCoreInstance;
+export const logViolations = _logViolations;
